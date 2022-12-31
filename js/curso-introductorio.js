a05d2c6e7139e618744cc6870f8681864f64c58d
Gera-MAJ <jatipg@gmail.com>
2022-12-31 14:56:19 -0300

    insertado el esqueleto de carga

--- js/curso-introductorio.js
+++ js/curso-introductorio.js
@@ -1,6 +1,10 @@
 window.onload = function(){
+    
     let conteiner = document.getElementById("conteiner-charge")
 
     conteiner.style.visibility = "hidden"
     conteiner.style.opacity = "0"
-} 
+}
+
+
+
