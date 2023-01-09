3cf40753cb420331254342aa1898e09d18b04e81
gera-maj <jatipg@gmail.com>
2023-01-09 14:18:56 -0300

    correciones

--- js/curso-introductorio.js
+++ js/curso-introductorio.js
@@ -1,9 +1,11 @@
 window.onload = function(){
-    
+      
     let conteiner = document.getElementById("conteiner-charge")
 
     conteiner.style.visibility = "hidden"
     conteiner.style.opacity = "0"
+
+    
 }
 
 
