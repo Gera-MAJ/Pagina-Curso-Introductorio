b24dddd0d0c3dd47fa45d9d7422af37b22108d1e
gera-maj <jatipg@gmail.com>
2023-01-09 23:42:22 -0300

    colocando lo que corresponde al scroll

--- js/curso-introductorio.js
+++ js/curso-introductorio.js
@@ -1,11 +1,26 @@
-window.onload = function(){
-      
-    let conteiner = document.getElementById("conteiner-charge")
+function windowCharge(){
+    const scroll = window.scrollY
+        if (scroll > 0){
+            window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth" });
+        }     
 
+}
+
+windowCharge();
+
+window.onload = function(){
+
+    const conteiner = document.getElementById("conteiner-charge")
     conteiner.style.visibility = "hidden"
     conteiner.style.opacity = "0"
 
-    
+
+    console.log(scroll)
+
+
 }
 
 
